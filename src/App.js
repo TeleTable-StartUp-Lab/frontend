ab0bfe2a504fadc8c966f5b1a346cd2e08e6385a
Desktop <stbuctob@bx.fallmerayer.it>
2025-11-22 12:20:38 +0100

    better animation while switching page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Layout from './components/layout/Layout';
-import ProtectedRoute from './components/layout/ProtectedRoute';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import Diary from './pages/Diary';
+import AnimatedRoutes from './components/layout/AnimatedRoutes';
 import BackendHealthCheck from './components/common/BackendHealthCheck';
 
 function App() {
@@ -16,30 +11,7 @@ function App() {
       <Router>
         <BackendHealthCheck />
         <Layout>
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/diary"
-              element={
-                <ProtectedRoute>
-                  <Diary />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
+          <AnimatedRoutes />
         </Layout>
       </Router>
     </AuthProvider>
---
+++ src/components/layout/AnimatedRoutes.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import ProtectedRoute from './ProtectedRoute';
+import PageTransition from './PageTransition';
+import Landing from '../../pages/Landing';
+import Login from '../../pages/Login';
+import Register from '../../pages/Register';
+import Dashboard from '../../pages/Dashboard';
+import Diary from '../../pages/Diary';
+
+const AnimatedRoutes = () => {
+    const location = useLocation();
+
+    return (
+        <AnimatePresence mode="wait">
+            <Routes location={location} key={location.pathname}>
+                <Route
+                    path="/"
+                    element={
+                        <PageTransition>
+                            <Landing />
+                        </PageTransition>
+                    }
+                />
+                <Route
+                    path="/login"
+                    element={
+                        <PageTransition>
+                            <Login />
+                        </PageTransition>
+                    }
+                />
+                <Route
+                    path="/register"
+                    element={
+                        <PageTransition>
+                            <Register />
+                        </PageTransition>
+                    }
+                />
+
+                <Route
+                    path="/dashboard"
+                    element={
+                        <ProtectedRoute>
+                            <PageTransition>
+                                <Dashboard />
+                            </PageTransition>
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/diary"
+                    element={
+                        <ProtectedRoute>
+                            <PageTransition>
+                                <Diary />
+                            </PageTransition>
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </AnimatePresence>
+    );
+};
+
+export default AnimatedRoutes;
