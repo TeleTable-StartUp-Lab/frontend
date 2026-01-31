56b7883af3af826296be62983105eff6cb5cdb55
Tobi <stbuctob@bx.fallmerayer.it>
2026-01-31 13:38:06 +0100

    top right shows role not username

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -145,7 +145,7 @@ const Navbar = () => {
                                             <User className="w-3.5 h-3.5 text-dark-900" />
                                         </div>
                                         <div className="text-left">
-                                            <div className="text-xs font-medium text-white">{user.username}</div>
+                                            <div className="text-xs font-medium text-white">{user.name}</div>
                                             <div className="text-[10px] text-gray-500 capitalize">{user.role}</div>
                                         </div>
                                     </button>
@@ -256,7 +256,7 @@ const Navbar = () => {
                                                 <User className="w-5 h-5 text-dark-900" />
                                             </div>
                                             <div>
-                                                <div className="text-sm font-medium text-white">{user.username}</div>
+                                                <div className="text-sm font-medium text-white">{user.name}</div>
                                                 <div className="text-xs text-gray-500 capitalize">{user.role}</div>
                                             </div>
                                         </div>
